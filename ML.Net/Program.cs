8edc40b3c9d385f944c3df40da2d076ee6e6f20f
Thidas Senavirathna <thidaskaveesha@gmail.com>
2025-03-06 18:25:20 +0530

    FEAT: Added ML .Net application

---
+++ ML.Net/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ML_Net;
+
+//Load sample data
+var sampleData = new MLModelTest.ModelInput()
+{
+    Col0 = @"Crust is not good.",
+};
+
+//Load model and predict output
+var result = MLModelTest.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+
